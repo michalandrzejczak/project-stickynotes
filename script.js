4b97f79ab03e29f8f5d35c04c185b2ac95cac5eb
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-04-09 15:01:04 +0200

    Added possibility to drag sticky notes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function() {
     
     var addStickynoteBtn = document.querySelector('.addNote');
-    
+
     addStickynoteBtn.addEventListener('click', createStickynote);
     
     function createStickynote() {
@@ -22,8 +22,43 @@
     
         document.body.appendChild(stickynoteElement);
         
+        stickynoteElement.setAttribute("draggable", "true");
+        
+        stickynoteElement.addEventListener('dragstart', drag_start, false); 
+        
     }
     
+    createStickynote();
     
+    document.body.addEventListener('dragover', drag_over, false); 
+    document.body.addEventListener('drop', drop, false); 
+    
+    function drag_start(event) {
+        
+        var style = window.getComputedStyle(event.target, null);
+        
+        event.dataTransfer.setData("text/plain",
+        (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
+        
+    } 
+    
+    function drag_over(event) { 
+        
+        event.preventDefault(); 
+        return false; 
+        
+    } 
+    
+    function drop(event) { 
+        
+        var offset = event.dataTransfer.getData("text/plain").split(',');
+        var draggableElement = document.querySelector('.stickynote:active');
+        draggableElement.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+        draggableElement.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+        event.preventDefault();
+        return false;
+        
+        } 
+
     
 })();
